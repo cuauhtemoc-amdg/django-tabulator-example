5aa47cb7c863dd4043ba78b8ef2673916d7efd59
Adrian Hernandez <temoc@amdg-tech.com>
2018-04-16 17:39:12 -0600

    Add ContactsList URL

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.urls import re_path
 from contacts.views import index
-#from contacts.views import ContactsList
+from contacts.views import ContactsList
 #from contacts.views import ContactsDetail
 
 
 urlpatterns = [
     path('', index, name='index'),
+    re_path('api/?$', ContactsList.as_view(), name='list'),
 ]
